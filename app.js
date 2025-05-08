a9928a300a18dc8f2a11168e3c60e7e7c413d4b1
AaradhyaGitHub <aaradhyagithub@gmail.com>
2025-05-08 12:21:16 -0700

    Sending first response

--- app.js
+++ app.js
@@ -6,7 +6,13 @@ const http = require("http");
 
 // this is how you create a server
 const server = http.createServer((req, res) => {
-  console.log(req);
+  console.log(req.url, req.method, req.headers);
+  // process.exit();  this kills the ongoing event loop
+
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html> <p>Hello from my node js server</p> </html>");
+  res.end();
+  
 });
 
 // listen will keep this running to listen for incoming request
