0db9bd6cf2367df36508377529f8b646655e8b55
AaradhyaGitHub <aaradhyagithub@gmail.com>
2025-05-14 20:22:10 -0700

    Migrating to handlebars

--- Unit-1/app.js
+++ Unit-1/app.js
@@ -1,20 +1,30 @@
+const path = require("path");
+
 const express = require("express");
 const bodyParser = require("body-parser");
-const path = require("path");
+const expressHbs = require("express-handlebars");
+
+const app = express();
+
+
+// handlebars setup
+// if hbs, file has to end in .hbs
+// if handlebars, file ends in .handlebars
+app.engine("hbs", expressHbs());
+app.set("view engine", "hbs");
+// pug setup
+// app.set("view engine", "pug");
+app.set("views", "views");
 
 const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
-const app = express();
-
-app.set("view engine", "pug");
-app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/admin", adminData.routes);
-app.use("/", shopRoutes);
+app.use(shopRoutes);
 
 app.use((req, res, next) => {
   res.render("404", { pageTitle: "Not Found :(" });
