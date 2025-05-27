41858a5cc0c0ee0cb4d1adc07f57e194b345f2a5
AaradhyaGitHub <aaradhyagithub@gmail.com>
2025-05-26 18:22:07 -0700

    Upgraded products page to render from SQL

--- TemplateEngines-MVCs/controllers/shop.js
+++ TemplateEngines-MVCs/controllers/shop.js
@@ -2,16 +2,15 @@ const Product = require("../models/product");
 const Cart = require("../models/cart");
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll((products) => {
-    res.render("shop/product-list", {
-      prods: products,
-      pageTitle: "All Products",
-      path: "/products",
-      hasProducts: products.length > 0,
-      activeShop: true,
-      productCSS: true
-    });
-  });
+  Product.fetchAll()
+    .then(([rows, fieldData]) => {
+      res.render("shop/product-list", {
+        prods: rows,
+        pageTitle: "All Products",
+        path: "/products"
+      });
+    })
+    .catch((err) => console.log(err));
 };
 
 exports.getProductDetail = (req, res, next) => {
