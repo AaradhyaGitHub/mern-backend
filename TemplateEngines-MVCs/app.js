f54a34b0efc19b40ce9a6555595475a60742b02e
AaradhyaGitHub <aaradhyagithub@gmail.com>
2025-05-28 17:22:01 -0700

    Fetching Related Products

--- TemplateEngines-MVCs/app.js
+++ TemplateEngines-MVCs/app.js
@@ -77,7 +77,7 @@ Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
 User.hasMany(Product);
 
 sequelize
-  .sync({ force: true })
+  .sync()
   .then((result) => {
     return User.findByPk(1);
   })
